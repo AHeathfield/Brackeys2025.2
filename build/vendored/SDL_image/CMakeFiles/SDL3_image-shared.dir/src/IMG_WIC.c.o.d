9cbaaaf29ad5482a056fefd70e14a7d491c6f198
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-08-24 22:51:47 -0400

    removed build folder

--- build/vendored/SDL_image/CMakeFiles/SDL3_image-shared.dir/src/IMG_WIC.c.o.d
+++
@@ -1,2 +0,0 @@
-vendored/SDL_image/CMakeFiles/SDL3_image-shared.dir/src/IMG_WIC.c.o: \
-  /Users/aidanheathfield/Documents/Game\ Development/SDL3_Games/Brackeys2025.2/vendored/SDL_image/src/IMG_WIC.c
--- build/vendored/SDL_mixer/CMakeFiles/SDL3_mixer-shared.dir/src/codecs/music_gme.c.o.d
+++
@@ -1,2 +0,0 @@
-vendored/SDL_mixer/CMakeFiles/SDL3_mixer-shared.dir/src/codecs/music_gme.c.o: \
-  /Users/aidanheathfield/Documents/Game\ Development/SDL3_Games/Brackeys2025.2/vendored/SDL_mixer/src/codecs/music_gme.c
